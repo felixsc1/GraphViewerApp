e7ba1de5b82df0193692a7ed07a72f3f67eb428b
Felix <felix.schlegel@auticon.ch>
2024-03-04 10:22:35 +0100

    add output logfile

--- run.ps1
+++ run.ps1
@@ -1,14 +1,16 @@
 $applicationPath = "C:\GitRepos\GraphViewerApp\app.py"
 $streamlitModule = "C:\GitRepos\GraphViewerApp\venv\Scripts\streamlit.exe"
 $uniqueIdentifier = "*streamlit*run*$applicationPath*"
+$logFile = "C:\GitRepos\GraphViewerApp\app_log.txt"
 
 while ($true) {
     # Use Get-WmiObject to check if a specific Streamlit application is running
     $running = Get-WmiObject Win32_Process | Where-Object { $_.CommandLine -like $uniqueIdentifier }
     if (-not $running) {
         Write-Host "Starting application..."
-        & $streamlitModule run $applicationPath
-        Write-Host "Application restarted at $(Get-Date)"
+        # Redirecting both standard output and error output to the log file
+        & $streamlitModule run $applicationPath *>> $logFile 2>&1
+        Write-Host "Application restarted at $(Get-Date)" *>> $logFile
     }
-    Start-Sleep -Seconds 10
+    Start-Sleep -Seconds 30
 }
